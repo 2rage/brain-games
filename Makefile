8060ab5446d160d3df2b139aefe62b6297718a23
2rage <the2rage@icloud.com>
2024-06-05 11:48:26 +0300

    complete step 5. version 0.2.0

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ install:
 brain-games:
 	poetry run brain-games
 
+brain-even:
+	poetry run brain-even
 
 build:
 	poetry build
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,49 @@
+import prompt
+import random
+from brain_games.cli import welcome_user
+from brain_games.scripts.brain_games import greet
+
+
+def even_number() -> int:
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    try_counter = 3
+
+    while try_counter > 0:
+        random_number = random.randrange(1, 99)
+        print(f'Question: {random_number}')
+
+        correct_answer = is_even(random_number)
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer != 'yes' and user_answer != 'no':
+            print('Error. Please enter answer "yes" or "no"')
+            break
+
+        if correct_answer == user_answer:
+            print('Correct!')
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            break
+        try_counter -= 1
+
+    if try_counter == 0:
+        print("Congratulations, Kostya!")
+
+    return
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def main():
+    greet()
+    welcome_user()
+    even_number()
+
+
+if __name__ == '__main__':
+    main()
